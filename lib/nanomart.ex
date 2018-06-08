252b937c2eb1e6a3b1504819b7ecd7e46f2643ba
Gary Button <garyhbutton@gmail.com>
2018-06-07 17:45:32 -0700

    Add restrictions

--- lib/nanomart.ex
+++ lib/nanomart.ex
@@ -2,24 +2,104 @@ defmodule NanomartError do
   defexception message: "Some error happened"
 end
 
+defmodule Restriction do
+  @drinking_age 21
+  @smoking_age 18
+
+  def drinking_age do
+    @drinking_age
+  end
+
+  def smoking_age do
+    @smoking_age
+  end
+
+  defmodule SmokingAge do
+    defstruct [:prompter]
+
+    def ck(restriction) do
+      age = restriction.prompter.get_age()
+
+      if age >= Restriction.smoking_age do
+        true
+      else
+        false
+      end
+    end
+  end
+
+  defmodule DrinkingAge do
+    defstruct [:prompter]
+
+    def ck(restriction) do
+      age = restriction.prompter.get_age()
+
+      if age >= Restriction.drinking_age do
+        true
+      else
+        false
+      end
+    end
+  end
+
+  def ck(restriction) do
+    case restriction do
+      %SmokingAge{} ->
+        SmokingAge.ck(restriction)
+      %DrinkingAge{} ->
+        DrinkingAge.ck(restriction)
+    end
+  end
+end
+
+
 defmodule Item do
   defmacro __using__(_opts) do
     quote do
-      defstruct [:output, :prompter]
+      defstruct [:logfile, :prompter]
 
-      def restrictions do
+      def rstrctns(item) do
         []
       end
 
-      defoverridable [restrictions: 0]
+      defoverridable [rstrctns: 1]
     end
+  end
+
+  def try_purchase(success) do
+    if success do
+      true
+    else
+      raise NanomartError
+    end
+  end
+
+  def log_sale(item) do
+    File.open(item.logfile, [:append], fn(file) ->
+      name = Item.name(item) |> Atom.to_string
+      IO.write(file, name <> "\n")
+    end)
+
+    true
+  end
+
+  # The common case naming doesn't work for miso soup
+  def name(%MisoSoup{}) do
+    :miso_soup
+  end
+
+  def name(item) do
+    class = item.__struct__ |> Atom.to_string
+    short_class = Regex.replace(~r/Elixir\.Item\./, class, "")
+    lower_short_class = String.downcase(short_class)
+    lower_short_class |> String.to_atom
   end
 end
 
 defmodule Item.Cola do
   use Item
 
-  def restrictions do
+  def rstrctns(cola) do
     []
   end
 end
@@ -27,7 +107,7 @@ end
 defmodule Item.MisoSoup do
   use Item
 
-  def restrictions do
+  def rstrctns(miso) do
     []
   end
 end
@@ -35,24 +115,24 @@ end
 defmodule Item.Beer do
   use Item
 
-  def restrictions do
-    [true]
+  def rstrctns(beer) do
+    [%Restriction.DrinkingAge{prompter: beer.prompter}]
   end
 end
 
 defmodule Item.Whiskey do
   use Item
 
-  def restrictions do
-    [true]
+  def rstrctns(whiskey) do
+    [%Restriction.DrinkingAge{prompter: whiskey.prompter}]
   end
 end
 
 defmodule Item.Cigarettes do
   use Item
 
-  def restrictions do
-    [true]
+  def rstrctns(cigarettes) do
+    [%Restriction.SmokingAge{prompter: cigarettes.prompter}]
   end
 end
 
@@ -61,27 +141,28 @@ defmodule Nanomart do
   Nanomart for selling items.
   """
 
-  defstruct [:output, :prompter]
+  defstruct [:logfile, :prompter]
 
   def sell_me(%__module__{} = nanomart, item_name) do
     item = case item_name do
       :cola ->
-        %Item.Cola{output: nanomart.output, prompter: nanomart.prompter}
+        %Item.Cola{logfile: nanomart.logfile, prompter: nanomart.prompter}
       :miso_soup ->
-        %Item.MisoSoup{output: nanomart.output, prompter: nanomart.prompter}
+        %Item.MisoSoup{logfile: nanomart.logfile, prompter: nanomart.prompter}
       :beer ->
-        %Item.Beer{output: nanomart.output, prompter: nanomart.prompter}
+        %Item.Beer{logfile: nanomart.logfile, prompter: nanomart.prompter}
       :whiskey ->
-        %Item.Whiskey{output: nanomart.output, prompter: nanomart.prompter}
+        %Item.Whiskey{logfile: nanomart.logfile, prompter: nanomart.prompter}
       :cigarettes ->
-        %Item.Cigarettes{output: nanomart.output, prompter: nanomart.prompter}
+        %Item.Cigarettes{logfile: nanomart.logfile, prompter: nanomart.prompter}
     end
 
-    Enum.map(item.__struct__.restrictions, fn(restriction) ->
-      raise NanomartError
+    Enum.map(item.__struct__.rstrctns(item), fn(rstrctn) ->
+      Restriction.ck(rstrctn)
+      |> Item.try_purchase
     end)
 
-    true
+    Item.log_sale(item)
   end
 end
 
--- test/nanomart_test.exs
+++ test/nanomart_test.exs
@@ -8,14 +8,33 @@ defmodule NanomartTest do
     end
   end
 
-  test "make sure the customer is old enough" do
-    nanomart = %Nanomart{output: "/dev/null", prompter: Age9}
+  defmodule Age19 do
+    def get_age do
+      19
+    end
+  end
 
-    assert Nanomart.sell_me(nanomart, :cola) == true
-    assert Nanomart.sell_me(nanomart, :miso_soup) == true
+  describe "make sure the customer is old enough" do
+    test "when you're a kid" do
+      nanomart = %Nanomart{logfile: "/dev/null", prompter: Age9}
 
-    assert_raise NanomartError, fn -> Nanomart.sell_me(nanomart, :beer) end
-    assert_raise NanomartError, fn -> Nanomart.sell_me(nanomart, :whiskey) end
-    assert_raise NanomartError, fn -> Nanomart.sell_me(nanomart, :cigarettes) end
+      assert Nanomart.sell_me(nanomart, :cola) == true
+      assert Nanomart.sell_me(nanomart, :miso_soup) == true
+
+      assert_raise NanomartError, fn -> Nanomart.sell_me(nanomart, :beer) end
+      assert_raise NanomartError, fn -> Nanomart.sell_me(nanomart, :whiskey) end
+      assert_raise NanomartError, fn -> Nanomart.sell_me(nanomart, :cigarettes) end
+    end
+
+    test "when you're a newly minted adult" do
+      nanomart = %Nanomart{logfile: "/dev/null", prompter: Age19}
+
+      assert Nanomart.sell_me(nanomart, :cola) == true
+      assert Nanomart.sell_me(nanomart, :miso_soup) == true
+      assert Nanomart.sell_me(nanomart, :cigarettes) == true
+
+      assert_raise NanomartError, fn -> Nanomart.sell_me(nanomart, :beer) end
+      assert_raise NanomartError, fn -> Nanomart.sell_me(nanomart, :whiskey) end
+    end
   end
 end
